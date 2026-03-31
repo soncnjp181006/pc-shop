b6ed65f275683c2169c8d812ea7c323facc6ea80
soncnjp181006 <tranxuansoncnjp181006@gmail.com>
2026-03-31 17:44:57 +0700

    Tạo Alembic & User Model thành công

---
+++ app/models/user.py
@@ -0,0 +1,36 @@
+"""
+User Model 
+
+Đại diện cho bảng user trong DB
+"""
+
+from app.db.base import Base # Base dùng để tạo Model
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime
+)
+from datetime import datetime
+from sqlalchemy import func
+import enum # enum.Enum -> Giá trị mặc định (Dùng trong phân quyền người dùng)
+
+class UserRole(str, enum.Enum):
+    """Phân quyền người dùng"""
+    CUSTOMER='CUSTOMER'
+    SELLER='SELLER'
+    ADMIN='ADMIN'
+
+class User(Base):
+    '''User Model'''
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(200), nullable=False, unique=True, index=True)
+    hashed_password = Column(String(200), nullable=False, unique=False)
+    is_active = Column(Boolean, nullable=False, default=True)
+    role = Column(String(20), nullable=False, default=UserRole.CUSTOMER.value)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email})>"
---
+++ tests/integration/user/test_user_model.py
@@ -0,0 +1,64 @@
+"""
+Test User model + migration
+
+Mục tiêu: 
+    - Đảm bảo bảng users đã được tạo trong DB
+    - Bảng users có đầy đủ các cột cần thiết
+"""
+
+from sqlalchemy import text
+from app.db.session import engine
+
+def test_users_table_exists():
+    """
+    Test case 1: Kiểm tra bảng users có tồn tại trong DB hay không
+        - Query trực tiếp vào DB
+        - Nếu bảng không tồn tại -> MySQL sẽ throw lỗi
+        - Nếu tồn tại -> Query chạy OK
+    """
+
+    with engine.connect() as connection:
+        result = connection.execute(
+            text("SHOW TABLES LIKE 'users';") # Hiện bảng user (LIKE '%_%')
+        )
+
+        table = result.fetchone() # Lấy dòng đầu tiên
+
+        # Đảm bảo bảng users tồn tại
+        assert table is not None
+
+def test_users_table_columns():
+    """
+    Test case 2: Kiểm tra bảng users có đầy đủ các cột hay không
+        - Dùng SQL: SHOW COLUMNS FROM users;
+        - Lấy danh sách cột
+        - So sánh kết quả mong đợi
+    """
+
+    with engine.connect() as connection:
+        # Lấy tất cả các cột trong bảng user
+        result = connection.execute(
+            text("SHOW COLUMNS FROM users;")
+        )
+
+        # result.fetchall() -> list các dòng
+        # mỗi dòng chứa info của một column
+        # index [0] là tên cột ở mỗi dòng
+        columns = result.fetchall()
+
+        # Lấy tên cột (Field)
+        column_names = [col[0] for col in columns]
+
+        # List danh sách các cột trong một user cơ bản
+        # Danh sách cột mong đợi có đầy đủ
+        expected_columns = [
+            'id', 'email', 'hashed_password',
+            'is_active', 'role', 'created_at'
+        ]
+
+        # Kiểm tra từng cột có tồn tại không
+        # Cách 1: Lấy từng giá trị trong expected_name để xem có nằm trong column_names
+        #   for col in expected_columns:
+        #       assert col in column_names
+        # Cách 2: Sắp xếp 2 danh sách và so sánh
+        assert sorted(column_names) == sorted(expected_columns)
