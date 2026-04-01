bb70fa14dc82116c7f4ce8ab717784ef6c8a8877
soncnjp181006 <tranxuansoncnjp181006@gmail.com>
2026-04-01 13:44:09 +0700

    Tách riêng repo của get_user_by_id ra khỏi service

---
+++ app/repositories/user_repo/userid.py
@@ -0,0 +1,5 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+
+def get_user_by_id_repo(db:Session, user_id:int):
+    return db.query(User).filter(User.id == user_id).first()
--- app/services/user_service/userid.py
+++ app/services/user_service/userid.py
@@ -6,4 +6,6 @@ def get_user_by_id(db:Session, user_id:int):
     Tìm user theo id
     """
 
-    return db.query(User).filter(User.id == user_id).first()
+    # return db.query(User).filter(User.id == user_id).first()
+    from app.repositories.user_repo.userid import get_user_by_id_repo
+    get_user_by_id_repo(db=db, user_id=user_id)
